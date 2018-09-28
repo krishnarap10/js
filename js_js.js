6d36969787942e3330f311824548142c8c4c3dd4
krishna123456789 <rapolkrishna10@gmail.com>
2018-09-27 21:59:58 -0400

    1st commit

--- js_js.js
+++ js_js.js
@@ -13,6 +13,7 @@ const companies= [
     {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
 ];
 
+
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 //imp methods:
